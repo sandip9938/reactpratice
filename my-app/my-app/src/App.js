41394ed5f2fc648122eabf7a4efaad518d1725cf
sandip9938 <sandippanda577@gmail.com>
2023-07-19 13:00:04 +0530

    My react pratice

---
+++ my-app/my-app/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <Navbar></Navbar>
+  );
+}
+
+export default App;
---
+++ my-app/my-app/src/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>I am a sandip who you are</h1>
+    </div>
+  )
+}
