cb30afe08a4be5ec5d716d5692e122be3f488ae4
sandip9938 <sandippanda577@gmail.com>
2023-07-27 19:53:29 +0530

    My react pratice

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     // Navbar
     <div className="navbar"> <Navbar></Navbar></div>
-      
+
  
   );
 }
--- my-app/my-app/src/Navbar.js
+++ my-app/my-app/src/Navbar.js
@@ -1,15 +1,22 @@
 import React from "react";
 
 export default function Navbar() {
+  
   return (
-    <div className="nav">
-      <ul>
-      <li><a href=""></a>HOME</li> 
-      <li><a href=""></a>ABOUT</li>
-      <li><a href=""></a>STORE</li>
-      <li><a href=""></a>CONTACt</li> 
-      </ul>
-      <img src="https://cdn.britannica.com/16/234216-050-C66F8665/beagle-hound-dog.jpg"alt="" />
-    </div>
+ <>
+
+<ul >
+  <li>Home</li>
+  <li>About</li>
+  <li>Service</li>
+  <li>Contact</li>
+</ul>
+
+
+
+
+ </>
+ 
+  
   )
-}
+ }
