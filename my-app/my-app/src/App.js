f77f23f15e3b7b428cb14f838f62b5638259b681
sandip9938 <sandippanda577@gmail.com>
2023-07-28 13:54:45 +0530

    reactjspratice

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Navbar from './Navbar';
+import Todo from './Todo';
 
 function App() {
   return (
     // Navbar
-    <div className="navbar"> <Navbar></Navbar></div>
-
+    <div className="navbar"> <Navbar></Navbar>
+    {/* Todo app  */}
+    <Todo></Todo>
+    </div>
+     
  
   );
 }
--- my-app/my-app/src/Navbar.js
+++ my-app/my-app/src/Navbar.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navbar() {
-  
+  const [x,setx]=useState(0)
+  const [y,sety]=useState(0)
+  const [z,setz]=useState(0)
   return (
  <>
+ count x: {x}
+<button onClick={()=> setx(x+1)}>Inc</button>
+<button onClick={()=> setx(x-1)}>Dec</button>
+<br />
+count y: {y}
+<button onClick={()=> sety(y+1)}>Inc</button>
+<button onClick={()=> sety(y-1)}>Dec</button>
 
-<ul >
-  <li>Home</li>
-  <li>About</li>
-  <li>Service</li>
-  <li>Contact</li>
-</ul>
+<br />
+
+count z: {z} is {(z%2==0)?'even':'odd'}
+<button onClick={()=> setz(z+1)}>Inc</button>
+<button onClick={()=> setz(z-1)}>Dec</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
