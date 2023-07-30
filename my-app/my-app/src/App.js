4ea3ce5828fc7c05c831fd519d12919e26ca934f
sandip9938 <sandippanda577@gmail.com>
2023-07-30 12:02:56 +0530

    reactjspratice

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
-import Navbar from './Navbar';
-import Todo from './Todo';
+
+import Prpos from './Prpos';
 
 function App() {
   return (
-    // Navbar
-    <div className="navbar"> <Navbar></Navbar>
-    {/* Todo app  */}
-    <Todo></Todo>
+    
+    <div> 
+    
+  <Prpos/>
     </div>
      
  
